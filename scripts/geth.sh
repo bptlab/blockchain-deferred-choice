42431685e3a07e9534980943445484d7d236fc01
Jan Ladleif <jan.ladleif@hpi.uni-potsdam.de>
2020-10-02 12:00:54 +0000

    Adapt geth setup, get rid of transaction pool on start

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -1,5 +1,6 @@
+rm ~/.ethereum/geth/transactions.rlp
 echo "y
 y
 y" | geth removedb
 geth init genesis.json
-geth --ws --mine --miner.threads=2 --miner.etherbase=0x0000000000000000000000000000000000000001
+geth --ws --nousb --cache 4096 --mine --miner.threads=2 --miner.gaslimit 80000000 --miner.etherbase=0x0000000000000000000000000000000000000001

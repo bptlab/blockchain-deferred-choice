81fa5889e73f22b451e8ffe1577fa15716c0abc1
jan.ladleif <jan.ladleif@hpi.uni-potsdam.de>
2020-10-09 14:55:31 +0200

    Minor adjustments to geth setup

--- scripts/geth.sh
+++ scripts/geth.sh
@@ -3,4 +3,4 @@ echo "y
 y
 y" | geth removedb
 geth init genesis.json
-geth --ws --nousb --cache 4096 --mine --miner.threads=2 --miner.gaslimit 80000000 --miner.etherbase=0x0000000000000000000000000000000000000001
+geth --ws --nousb --cache 4096 --mine --miner.threads=1 --miner.gaslimit 80000000 --miner.gastarget 800 --miner.etherbase=0x0000000000000000000000000000000000000001
